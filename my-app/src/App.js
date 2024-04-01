cddb867b88ddebf8d9a26db23421cb503f03952a
Chi Chen <stephchen55@gmail.com>
2024-03-31 23:27:26 -0400

    add spotlight effect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import gsap from 'gsap';
 
 function App() {
+  const [, setOverlayPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      const x = Math.round((clientX / window.innerWidth) * 100);
+      const y = Math.round((clientY / window.innerHeight) * 100);
+      setOverlayPosition({ x, y });
+
+      gsap.to('.overlay2', {
+        '--x': `${x}%`,
+        '--y': `${y}%`,
+        duration: 0.3,
+        ease: 'sine.out'
+      });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="overlay1">
+        <h1>WELCOME TO MY WORLD</h1>
+      </section>
+
+      <section class="overlay2">
+        <h1>WELCOME TO MY WORLD</h1>
+      </section>
     </div>
   );
 }
